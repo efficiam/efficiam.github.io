1148bb3fddcf6ca132eaeedadd017266fc776bdb
diegopq <diego.pq.k@gmail.com>
2016-11-04 17:26:59 +0100

    And now...FADE!

---
+++ src/js/main.js
@@ -0,0 +1,51 @@
+(function(document, window) {
+
+  var FadeOnScroll = function(opts) {
+    this.elements   = opts.elements || null;
+    this.threshold  = Number.isInteger(opts.threshold) ? opts.threshold : 5;
+
+    this.elements.forEach(function(element) {
+      _hide(element);
+    });
+  };
+
+  FadeOnScroll.prototype.init = function() {
+    var self = this;
+
+    if(!Array.isArray(this.elements))
+      return;
+
+    // First check
+    self.elements.forEach(function(element) {
+      if(_isOnScreen(element, self.threshold))
+        _show(element);
+    });
+
+    window.addEventListener('scroll', function() {
+      self.elements.forEach(function(element) {
+        if(_isOnScreen(element, self.threshold))
+          _show(element);
+      });
+    });
+  };
+
+  function _isOnScreen(element, threshold) {
+    var viewportHeight  = window.innerHeight;
+    var elementPosition = element.getBoundingClientRect().top;
+
+    return -(elementPosition - viewportHeight) >= threshold;
+  }
+
+  function _show(element) {
+    element.style.visibility  = 'visible';
+    element.style.opacity     = 1;
+  }
+
+  function _hide(element) {
+    element.style.visibility  = 'hidden';
+    element.style.opacity     = 0;
+  }
+
+  window.FadeOnScroll = FadeOnScroll;
+
+})(document, window);
