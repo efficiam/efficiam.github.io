3a74b60852c6e5ebf81f041c0b98b3291b23db2f
diegopq <diego.pq.k@gmail.com>
2016-11-08 17:51:17 +0100

    Added IE polyfill for isInteger

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+// IE polyfill for isInteger
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === "number" &&
+    isFinite(value) &&
+    Math.floor(value) === value;
+};
+
 (function(document, window) {
 
   var FadeOnScroll = function(opts) {
