7173fa1cee41009789ff5d0d9a9c70fd134c5f38
Riccardo Gueli Alletti <riccardo.gueli.alletti@gmail.com>
2016-11-20 16:54:51 +0100

    More options to generate poly and poly footer

--- src/js/main.js
+++ src/js/main.js
@@ -56,113 +56,136 @@ Number.isInteger = Number.isInteger || function(value) {
   window.FadeOnScroll = FadeOnScroll;
   
   // Three js to refactor
-  var windowWidth = window.outerWidth;
-  var windowHeight = window.outerHeight;
-  var container = document.querySelector('.u-bg-poly');
-  var width = container.getBoundingClientRect().width;
-  var height = container.getBoundingClientRect().height;
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-  var blue = new THREE.Color(0xbd19f5);
-  var violet = new THREE.Color(0x3c73db);
-  var gradientGeometry;
-  var gradientMaterial;
-  var gradientPlane;
-  var polyGeometry;
-  var polyMaterial;
-  var polyPlane;
-  var renderer = new THREE.WebGLRenderer();
+  addPolyTo(document.querySelector('.u-bg-poly'));
+  addPolyTo(document.querySelector('footer'), {size: 'small', lightPosition: 'up'});
+  
+  function addPolyTo(el, opts) {
+    var opts = opts || {};
+    var windowWidth = window.outerWidth;
+    var windowHeight = window.outerHeight;
+    var width = el.getBoundingClientRect().width;
+    var height = el.getBoundingClientRect().height;
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    var blue = new THREE.Color(0xbd19f5);
+    var violet = new THREE.Color(0x3c73db);
+    var gradientGeometry;
+    var gradientMaterial;
+    var gradientPlane;
+    var polyGeometry;
+    var polyMaterial;
+    var polyPlane;
+    var renderer = new THREE.WebGLRenderer();
 
-  renderer.setSize(width, height);
-  container.appendChild(renderer.domElement);
-  
-  var light = new THREE.PointLight();
-  light.position.set(0, -300, 250);
-  scene.add(light);
-  
-  camera.position.z = 12;  
-  var compound = function(x, time) {
-    return (Math.sin(x + time /500) + Math.sin(2.2 * x + time /500 - 2.3) + Math.sin(4.75 * x + time /500 + 8)) / 8
-  };
-  
-  var initialTime = new Date();
-  
-  function addPlanes(width, height) {
-    var ratio = width/height;
-    var round = Math.round;
-    var width = round(40 * ratio);
-    var height = round(40 * ratio);
-    var widthSegments = round(10 * ratio);
-    var heightSegments = round(10 * ratio);
-    gradientGeometry = new THREE.PlaneGeometry(width, height);
-    gradientMaterial = new THREE.MeshBasicMaterial({
-      vertexColors: THREE.VertexColors
-    });
+    renderer.setSize(width, height);
+    el.appendChild(renderer.domElement);
     
-    gradientPlane = new THREE.Mesh(gradientGeometry, gradientMaterial);
-    gradientPlane.position.z = -5
-    gradientPlane.rotation.z -= 1
+    var light = new THREE.PointLight();
+    var lightPosition; 
+    switch (opts.lightPosition) {
+      case 'left':
+        lightPosition = [300, 0, 250];
+        break;
+      case 'right':
+        lightPosition = [-300, 0, 250];
+        break;
+      case 'up':
+        lightPosition = [0, 300, 250];
+        break;
+      case 'down':
+      default:
+        lightPosition = [0, -300, 250];
+        
+    }
+    light.position.set.apply(light.position,lightPosition);
+    scene.add(light);
     
-    gradientGeometry.faces[0].vertexColors = [violet, violet, blue];
-    gradientGeometry.faces[1].vertexColors = [violet, blue, blue];
-    scene.add(gradientPlane);
+    camera.position.z = 12;  
+    var compound = function(x, time) {
+      return (Math.sin(x + time /500) + Math.sin(2.2 * x + time /500 - 2.3) + Math.sin(4.75 * x + time /500 + 8)) / 8
+    };
     
-    polyGeometry = new THREE.PlaneGeometry(
-      width, 
-      height,
-      widthSegments,
-      heightSegments
-    );
+    var initialTime = new Date();
+    
+    function addPlanes(width, height) {
+      var ratio = width/height;
+      var round = Math.round;
+      var baseSize = 40;
+      var baseSegments = opts.size === 'small' ? 7 : 10;
+      var width = round(baseSize * ratio);
+      var height = round(baseSize * ratio);
+      var widthSegments = round(baseSegments * ratio);
+      var heightSegments = round(baseSegments * ratio);
+      
+      gradientGeometry = new THREE.PlaneGeometry(width, height);
+      gradientMaterial = new THREE.MeshBasicMaterial({
+        vertexColors: THREE.VertexColors
+      });
+      
+      gradientPlane = new THREE.Mesh(gradientGeometry, gradientMaterial);
+      gradientPlane.position.z = -5
+      gradientPlane.rotation.z -= 1
+      
+      gradientGeometry.faces[0].vertexColors = [violet, violet, blue];
+      gradientGeometry.faces[1].vertexColors = [violet, blue, blue];
+      scene.add(gradientPlane);
+      
+      polyGeometry = new THREE.PlaneGeometry(
+        width, 
+        height,
+        widthSegments,
+        heightSegments
+      );
 
-    polyMaterial = new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      specular: 0xff0000,
-      shading: THREE.FlatShading,
-      blending: THREE.MultiplyBlending,
-      transparent: true
-    });
+      polyMaterial = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        specular: 0xff0000,
+        shading: THREE.FlatShading,
+        blending: THREE.MultiplyBlending,
+        transparent: true
+      });
+      
+      polyGeometry.vertices.forEach(function(v) {
+        v.x += (Math.random() - 0.5)*1.5;
+        v.y += (Math.random() - 0.5)*1.5;
+      });
+      
+      
+      polyPlane = new THREE.Mesh(polyGeometry, polyMaterial);
+      polyPlane.rotation.z -= 1
+      
+      scene.add(polyPlane);
+    }
     
-    polyGeometry.vertices.forEach(function(v) {
-      v.x += (Math.random() - 0.5)*1.5;
-      v.y += (Math.random() - 0.5)*1.5;
-    });
+    function render() {
+      requestAnimationFrame(render);
+      var newTime = new Date();
+      var time = (newTime - initialTime) / 3;
+      polyGeometry.vertices.forEach(function(v) {
+        v.z =  compound(v.x, time);
+      });
+      polyPlane.material.needsUpdate =true
+      polyPlane.geometry.verticesNeedUpdate = true;
+    	renderer.render(scene, camera);
+    }
     
+    addPlanes(width, height);
+    render();
     
-    polyPlane = new THREE.Mesh(polyGeometry, polyMaterial);
-    polyPlane.rotation.z -= 1
+    window.addEventListener('resize', onWindowResize, false);
     
-    scene.add(polyPlane);
+    function onWindowResize() {
+      if (windowWidth === window.outerWidth && 
+          windowHeight === window.outerHeight) return;
+      var newWidth = el.getBoundingClientRect().width;
+      var newHeight = el.getBoundingClientRect().height;
+      scene.children.pop();
+      scene.children.pop();
+      addPlanes(newWidth, newHeight);
+      camera.aspect = newWidth / newHeight;
+  		camera.updateProjectionMatrix();
+  		renderer.setSize(newWidth, newHeight);
+  	}  
   }
-  
-  function render() {
-    requestAnimationFrame(render);
-    var newTime = new Date();
-    var time = (newTime - initialTime) / 3;
-    polyGeometry.vertices.reverse().forEach(function(v) {
-      v.z =  compound(v.x/v.y, time);
-    });
-    polyPlane.material.needsUpdate =true
-    polyPlane.geometry.verticesNeedUpdate = true;
-  	renderer.render(scene, camera);
-  }
-  
-  addPlanes(width, height);
-  render();
-  
-  window.addEventListener('resize', onWindowResize, false);
-  
-  function onWindowResize() {
-    if (windowWidth === window.outerWidth && 
-        windowHeight === window.outerHeight) return;
-    var newWidth = container.getBoundingClientRect().width;
-    var newHeight = container.getBoundingClientRect().height;
-    scene.children.pop();
-    scene.children.pop();
-    addPlanes(newWidth, newHeight);
-    camera.aspect = newWidth / newHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(newWidth, newHeight);
-	}
-
 
 })(document, window);
